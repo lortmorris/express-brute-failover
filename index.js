ecf11df6ea9f68c5a561e1a5ac096e81e33045e6
Cesar Casas <cesarcasas@bsdsolutions.com.ar>
2016-12-20 10:21:14 -0300

    first version

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const redis = require('redis');
+const ExpressBrute = require('express-brute');
+const debug = require('debug')('express-brute-failover');
+const bruteRedisStore = require('express-brute-redis');
+
+function expresBruteFailoverMain(params){
+  params = params || {};
+  const redisOptions =  Object.assign({},
+    {host: '127.0.0.1', port: 6379},
+    params.redis || {});
+
+    let failCallback = (a,b)=>{
+      console.log(`se cago la fruta en brute: `, a,b);
+    };
+
+    const expressBruteOptions = Object.assign({},
+      {
+        freeRetries: 200,
+        lifetime: 5 * 60,
+        minWait: 500,
+        maxWait: 60000,
+        failCallback: failCallback
+      },
+      params.brute || {});
+
+      const expressBruteInstance = new ExpressBrute(expressBruteOptions, new ExpressBrute.MemoryStore());
+      const redisClient = redis.createClient( redisOptions );
+
+      //let expressBruteInstance = new ExpressBrute(new  bruteRedisStore({client: redisClient}) , expressBruteOptions);
+
+      redisClient.on('connect', ()=>{ });
+
+      redisClient.on('error', ()=>{
+        debug('redis.error called');
+        expressBruteInstance.store = new ExpressBrute.MemoryStore();
+      });
+
+      redisClient.on('end', ()=>{ debug('redis.end called'); });
+      redisClient.on('ready', ()=>{
+        debug('redis:: connected');
+        expressBruteInstance.store =  new  bruteRedisStore({client: redisClient});
+      });
+
+      return expressBruteInstance;
+    }
+
+module.exports = expresBruteFailoverMain;
--- test/index.js
+++ test/index.js
@@ -1,15 +1,5 @@
-# express-brute-failover
-This is a easy and simple version of express-brute with failover (when using redis/memory);
-
-# install
-```bash
-$ npm install express-brute-failover
-```
-
-# example
-```js
 const express = require('express');
-const EBF = require('express-brute-failover');
+const EBF = require('../index.js');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
@@ -38,8 +28,3 @@ app.get('/', (req, res, next)=> {
 server.listen(5000, ()=>{
   console.log(`listen on *:5000`);
 });
-
-```
-
-# Powered by
-[César Casas](https://ar.linkedin.com/in/cesarcasas)
